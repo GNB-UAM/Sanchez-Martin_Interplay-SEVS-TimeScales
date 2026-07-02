8c440ddc409bb9a5c280b7fc88851ccdce005efd
Pablo Sanmar <pablosanmar97@gmail.com>
2026-07-02 12:01:50 +0200

    log-log for Fig5 cycle by cycle

---
+++ Fig5/log_pairplots.py
@@ -0,0 +1,90 @@
+"""Copyright (c) 2026 Pablo Sanchez-Martin. All Rights Reserved.
+Use of this source code is govern by GPL-3.0 license that 
+can be found in the LICENSE file"""
+
+import pandas as pd
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import scipy.stats as stats
+
+#GENERAL CONSTANTS
+plt.rcParams.update({'font.size': 20})
+
+path = "../analyzed_data.pkl"
+df_data = pd.read_pickle(path)
+
+
+exp = "12"
+
+
+plt.rcParams.update({'font.size': 20})
+
+number_cycles = 1000
+
+
+x0, y0 = np.array(df_data[exp]["PD1"]["sdf_100_gauss"][:number_cycles])*10, np.array(df_data[exp]["PD2"]["sdf_100_gauss"][:number_cycles])*10
+x1, y1 = np.array(df_data[exp]["PD1"]["sdf_100_gauss"][:number_cycles])*10, np.array(df_data[exp]["LP"]["sdf_100_gauss"][1:number_cycles+1])*10
+x2, y2 = np.array(df_data[exp]["PD1"]["sdf_100_gauss"][:number_cycles])*10, np.array(df_data[exp]["intervals"]["LPPD1_delay"][:number_cycles])/10
+x3, y3 = np.array(df_data[exp]["intervals"]["PD1_period"][:number_cycles])/10, np.array(df_data[exp]["intervals"]["LPPD1_delay"][:number_cycles])/10
+
+
+fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(8, 12), sharex=False)
+
+
+axes[0].scatter(x0, y0, s = 5, c ="#555555")
+axes[0].set_xscale("log")
+axes[0].set_yscale("log")
+axes[0].set_xlabel("PD1 SDF (s⁻¹)")
+axes[0].set_ylabel("PD2 SDF (s⁻¹)")
+
+
+axes[1].scatter(x1, y1, s = 5, c ="#555555")
+axes[1].set_xscale("log")
+axes[1].set_yscale("log")
+axes[1].set_xlabel("PD1 SDF (s⁻¹)")
+axes[1].set_ylabel("LP SDF (s⁻¹)")
+
+
+axes[2].scatter(x2, y2, s = 5, c ="#555555")
+axes[2].set_xscale("log")
+axes[2].set_yscale("log")
+axes[2].set_xlabel("PD1 SDF (s⁻¹)")
+axes[2].set_ylabel("LPPD1 delay (ms)")
+
+
+axes[3].scatter(x3, y3, s = 5, c ="#555555")
+axes[3].set_xscale("log")
+axes[3].set_yscale("log")
+axes[3].set_xlabel("PD1 Period (ms)")
+axes[3].set_ylabel("LPPD1 delay (ms)")
+
+for ax in axes:
+    ax.set_box_aspect(1)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+
+plt.tight_layout()
+
+plt.savefig("log_pairplots.svg")
+
+
+
+fig, ax = plt.subplots(figsize = (8, 8))
+ax.scatter(x2, y2, s=3, alpha=0.7, edgecolors="k")
+# Set logarithmic scale on the x variable
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_ylim(50, 1000)
+ax.set_xlabel("PD1 SDF (s⁻¹)")
+ax.set_ylabel("LPPD1 delay (ms)")
+plt.tight_layout()
+plt.savefig("log_sdf_delay.svg")
+
+
+result = stats.spearmanr(x2, y2)
+print(f"Spearman Correlation Coefficient: {result.statistic:.2f}")
+print(f"p-value: {result.pvalue:.2e}")
+print(repr(result.pvalue))
